6440cce3eda5e19a8beb38507fbb1a88a79e84b0
Nanna <nanaa.gonzalez@gmail.com>
2024-07-03 18:30:45 +0200

    Add animations to pcb images

--- src/app/page.js
+++ src/app/page.js
@@ -8,11 +8,39 @@ import border from './assets/images/homePage/border.svg';
 import arrowRight from './assets/images/homePage/arrowRight.svg';
 import CheckWindowWidth from './hooks/useWindowWidth';
 import { useLanguageStore } from './stores/languageStore';
+import pcb1 from './assets/images/homePage/pcb1.webp';
+import pcb2 from './assets/images/homePage/pcb2.webp';
+import pcb3 from './assets/images/homePage/pcb3.webp';
+import pcb4 from './assets/images/homePage/pcb4.webp';
 import './home.css';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
   const { screenWidth } = CheckWindowWidth();
   const language = useLanguageStore((state) => state.language);
+  const images = [pcb1, pcb2, pcb3, pcb4];
+  const [img, setImg] = useState(images[0]);
+
+  useEffect(() => {
+    const pcb = document.querySelector('.pcb');
+
+    const animateZoom = () => {
+      pcb.classList.add('zoom');
+      setTimeout(() => {
+        pcb.classList.remove('zoom');
+      }, 10000);
+    };
+
+    animateZoom();
+    const carousel = setInterval(() => {
+      const currentIndex = images.indexOf(img);
+      const nextIndex = (currentIndex + 1) % images.length;
+      setImg(images[nextIndex]);
+      animateZoom();
+    }, 10000);
+
+    return () => clearInterval(carousel);
+  });
 
   return (
     <main>
@@ -27,6 +55,9 @@ export default function Home() {
         </div>
         <div className='border-zoom-image'>
           <Image src={screenWidth >= 1366 ? borderLarge : border} alt='Image container border' fill={true} className='border-image'/>
+          <div className='pcb-container'>
+            <Image src={img} alt='PCB' fill={true} className='pcb'/>
+          </div>
         </div>
         <div className='our-approach'>
           <p>{content[language].HomePage.stay}</p>
