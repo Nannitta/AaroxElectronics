f4be0b031f07d53332f273be60cf3e9d0974e1ce
Nanna <nanaa.gonzalez@gmail.com>
2024-07-09 13:18:43 +0200

    Add cookies page and responsive styles

--- src/app/cookies/page.jsx
+++ src/app/cookies/page.jsx
@@ -45,6 +45,8 @@ export default function Cookies() {
             <span>{cookies[language].own.title}</span>
             <p>{cookies[language].own.desc}</p>
           </li>
+        </ul>
+        <div className='cookies-table1'>
           <table>
             <thead>
               <tr>
@@ -68,37 +70,111 @@ export default function Cookies() {
               </tr>
             </tbody>
           </table>
-          <li>
-            <span>Third parties</span>
-            <p>Those installed by entities other than the Publisher or those where the information is managed by third parties, despite the cookies being installed from an equipment or domain managed by the Publisher. The Publisher has integrated add-ons by external providers, such as Google, Inc, on its website. These third parties and their cookies are governed by their own Terms and Conditions, stated previously when describing the types of cookies according to their purpose.</p>
-          </li>
-        </ul>
-        <p>Google, Inc. is located in the USA and the acceptance of the cookies associated to its services entails the international transfer of the data resulting from the cookies to said provider. This transfer is based on the user’s consent, which may be withdrawn at any time. According to the European authorities, US law does not guarantee an adequate level of data protection. Google is affiliated to the Web Choices tool, from which the use of its cookies can be disabled: <Link href={' https://optout.aboutads.info/'} target="_blank">here</Link>.</p>
-        <h3>COOKIES</h3>
-        <p>Cookies will be installed after obtaining the users’ consent, except for technical or necessary cookies, which will be installed in any case. Users may authorise cookie installation by clicking on “Accept” on the cookie notice.</p>
-        <p>Should the user wish to withdraw the consent given, he or she must delete the installed cookies and then choose “Reject” on the cookie notice. In order to prevent their future installation, it is possible to enable the Private Browsing or “Do-Not-Track” options on the browser, that is, to block or disable the installation of cookies.</p>
-        <p>The user must keep in mind that some features of the website content are only available if the installation of cookies on the browser is allowed. Should you decide to reject or block certain cookies, depending on their purpose, this may affect, totally or partially, the normal operation of the website or prevent access to some of its services.</p>
-        <p>In order to allow, block or delete the cookies installed on an equipment or device by configuring the options of the browser installed on it, the user must follow the instructions provided by the browser. These are the links for the configuration of cookies on the most popular browsers:</p>
+        </div>
         <ul>
           <li>
-            <p>Internet Explorer: <Link href={'https://support.microsoft.com/en-gb/windows/manage-cookies-in-microsoft-edge-view-allow-block-delete-and-use-168dab11-0753-043d-7c16-ede5947fc64d#ie=ie-10'} target="_blank">Delete and manage cookies</Link></p>
-          </li>
-          <li>
-            <p>Mozilla Firefox: <Link href={'https://support.mozilla.org/en-US/kb/clear-cookies-and-site-data-firefox'} target="_blank">Clear cookies</Link> and <Link href={'https://support.mozilla.org/en-US/kb/block-websites-storing-cookies-site-data-firefox'} target="_blank">block cookies</Link></p>
-          </li>
-          <li>
-            <p>Google Chrome: <Link href={'https://support.google.com/chrome/answer/95647?hl=en'} target="_blank">Clear, enable and manage cookies</Link></p>
-          </li>
-          <li>
-            <p>Opera: <Link href={'https://help.opera.com/en/latest/web-preferences/#cookies'} target="_blank">Cookie options</Link></p>
-          </li>
-          <li>
-            <p>Safari: <Link href={'https://support.apple.com/en-gb/guide/safari/sfri11471/mac'} target="_blank">Manage cookies</Link></p>
+            <span>{cookies[language].thirdParties.title}</span>
+            <p>{cookies[language].thirdParties.desc}</p>
           </li>
         </ul>
-        <p>If you use some other browser or if the provided links do not contain the desired information, you can learn how to manage the cookies installed on an equipment by accessing the “Options”, “Settings”, or equivalent menus on the browser, or by referring to the “Help” or “Support” sections on the browser. Some browsers allow the configuration of specific rules —exceptions— in order to manage cookies on a per-site basis, so that the user can disable cookies for all sites except for those he or she trusts.</p>
-        <p>On the other hand, if you wish to prevent Google Analytics from collecting data related to your access to the website, you can install the Google Analytics <Link href={'https://tools.google.com/dlpage/gaoptout?hl=en'} target="_blank">opt-out browser add-on</Link> The add-on does not stop the information from being sent to the website or, through other means, to other web analytics services. There are additional privacy configuration options for Google services, available through these links: <Link href={'https://safety.google/intl/en/privacy/privacy-controls/'} target="_blank">Privacy Controls</Link> Google and <Link href={'https://policies.google.com/technologies/partner-sites?hl=en'} target="_blank">Privacy partners Google</Link>.</p>
-        <p>The user must set his or her preferences on all the devices he or she uses to browse the Internet.</p>
+        <div className='cookies-table2'>
+          <table>
+            <thead>
+              <tr>
+                <th>{cookies[language].thirdParties.table.thirdParty}</th>
+                <th>{cookies[language].thirdParties.table.purpose.title}</th>
+                <th>{cookies[language].thirdParties.table.name}</th>
+                <th>{cookies[language].thirdParties.table.description}</th>
+                <th>{cookies[language].thirdParties.table.duration}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td rowSpan={11}>{cookies[language].thirdParties.table.descThirdParty}</td>
+                <td rowSpan={4}>{cookies[language].thirdParties.table.purpose.technical.title}</td>
+                <td>SID Y HSID</td>
+                <td>{cookies[language].thirdParties.table.purpose.technical.sidDesc}</td>
+                <td>{cookies[language].thirdParties.table.purpose.technical.sidDuration}</td>
+              </tr>
+              <tr>
+                <td>SSID, APISID y SAPISID</td>
+                <td>{cookies[language].thirdParties.table.purpose.technical.ssidDesc}</td>
+                <td>{cookies[language].thirdParties.table.purpose.technical.ssidDuration}</td>
+              </tr>
+              <tr>
+                <td>OGPC</td>
+                <td>{cookies[language].thirdParties.table.purpose.technical.ogpcDesc}</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>RC::C</td>
+                <td>{cookies[language].thirdParties.table.purpose.technical.rcDesc}</td>
+                <td>{cookies[language].thirdParties.table.purpose.technical.rcDuration}</td>
+              </tr>
+              <tr>
+                <td rowSpan={3}>{cookies[language].thirdParties.table.purpose.analytics.title}</td>
+                <td>_ga y _ga_XXXXXXXXXX</td>
+                <td rowSpan={3}>{cookies[language].thirdParties.table.purpose.analytics.gDesc}</td>
+                <td>{cookies[language].thirdParties.table.purpose.analytics.gaDuration}</td>
+              </tr>
+              <tr>
+                <td>_gid</td>
+                <td>{cookies[language].thirdParties.table.purpose.analytics.gidDuration}</td>
+              </tr>
+              <tr>
+                <td>_gat_UA-XXXXXXXXX_X</td>
+                <td>{cookies[language].thirdParties.table.purpose.analytics.gatDuration}</td>
+              </tr>
+              <tr>
+                <td rowSpan={4}>{cookies[language].thirdParties.table.purpose.marketing.title}</td>
+                <td>1P_JAR</td>
+                <td>{cookies[language].thirdParties.table.purpose.marketing.jarDesc}</td>
+                <td>{cookies[language].thirdParties.table.purpose.marketing.jarDuration}</td>
+              </tr>
+              <tr>
+                <td>NID</td>
+                <td>{cookies[language].thirdParties.table.purpose.marketing.nidDesc}</td>
+                <td>{cookies[language].thirdParties.table.purpose.marketing.nidDuration}</td>
+              </tr>
+              <tr>
+                <td>__Secure-ENID</td>
+                <td>{cookies[language].thirdParties.table.purpose.marketing.enidDesc}</td>
+                <td>{cookies[language].thirdParties.table.purpose.marketing.enidDuration}</td>
+              </tr>
+              <tr>
+                <td>__Secure-1PSID y __Secure-1PAPISID, __Secure-3PSID y __Secure-3PAPISID</td>
+                <td>{cookies[language].thirdParties.table.purpose.marketing.psidDesc}</td>
+                <td>{cookies[language].thirdParties.table.purpose.marketing.psidDuration}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <p>{cookies[language].sixthParagraph} <Link href={' https://optout.aboutads.info/'} target="_blank">{cookies[language].linkTitle}</Link>.</p>
+        <h3>{cookies[language].cookies.title}</h3>
+        <p>{cookies[language].cookies.firstParagraph}</p>
+        <p>{cookies[language].cookies.secondParagraph}</p>
+        <p>{cookies[language].cookies.thirdParagraph}</p>
+        <p>{cookies[language].cookies.fourthParagraph}</p>
+        <ul>
+          <li>
+            <p>Internet Explorer: <Link href={cookies[language].cookies.browser.linkExplorer} target="_blank">{cookies[language].cookies.browser.explorer}</Link></p>
+          </li>
+          <li>
+            <p>Mozilla Firefox: <Link href={cookies[language].cookies.browser.mozilla.linkClear} target="_blank">{cookies[language].cookies.browser.mozilla.clear}</Link> <Link href={cookies[language].cookies.browser.mozilla.linkBlock} target="_blank">{cookies[language].cookies.browser.mozilla.block}</Link></p>
+          </li>
+          <li>
+            <p>Google Chrome: <Link href={cookies[language].cookies.browser.linkChrome} target="_blank">{cookies[language].cookies.browser.chrome}</Link></p>
+          </li>
+          <li>
+            <p>Opera: <Link href={cookies[language].cookies.browser.linkOpera} target="_blank">{cookies[language].cookies.browser.opera}</Link></p>
+          </li>
+          <li>
+            <p>Safari: <Link href={cookies[language].cookies.browser.linkSafari} target="_blank">{cookies[language].cookies.browser.safari}</Link></p>
+          </li>
+        </ul>
+        <p>{cookies[language].cookies.fifthParagraph}</p>
+        <p>{cookies[language].cookies.sixthParagraph} <Link href={cookies[language].cookies.linkAddon} target="_blank">{cookies[language].cookies.addon}</Link> {cookies[language].cookies.seventhParagraph} <Link href={cookies[language].cookies.linkPrivacy} target="_blank">{cookies[language].cookies.privacy}</Link> {cookies[language].cookies.and} <Link href={cookies[language].cookies.linkPartners} target="_blank">{cookies[language].cookies.partners}</Link>.</p>
+        <p>{cookies[language].cookies.eightParagraph}</p>
       </section>
     </main>
   )
