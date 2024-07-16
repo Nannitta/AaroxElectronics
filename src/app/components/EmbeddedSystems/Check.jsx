c99da6f5d481e29e673af3aaafd0f34c7fb7ffb1
Nanna <nanaa.gonzalez@gmail.com>
2024-07-16 11:48:01 +0200

    Add resposive styles to embeddedSystems page

--- src/app/components/EmbeddedSystems/Check.jsx
+++
@@ -1,7 +0,0 @@
-export default function Check({ height, width, value }) {
-  return(
-    <svg xmlns="http://www.w3.org/2000/svg" height={height} width={width} viewBox="0 0 118.43873 118.43873">
-      <path className={`check ${value}`} strokeLinejoin="round" d="M34.682 60.352l15.61 15.61 33.464-33.464" stroke="#2dac44" strokeLinecap="round" strokeWidth="5" fill="none"/>
-    </svg>
-  )
-}
--- src/app/embedded-systems/page.jsx
+++ src/app/embedded-systems/page.jsx
@@ -5,9 +5,7 @@ import content from '../content.json';
 import './embedded.css';
 import CheckWindowWidth from '../hooks/useWindowWidth';
 import { screenSizes } from '../lib/screenSizes';
-import Check from '../components/EmbeddedSystems/Check';
-/* import Image from 'next/image';
-import embedded1 from '../assets/images/embedded/embedded1.webp'; */
+import FirstSection from './firstSection/FirstSection';
 
 export default function EmbeddedSystems() {
   const language = useLanguageStore((state) => state.language);
@@ -15,67 +13,23 @@ export default function EmbeddedSystems() {
 
   return(
     <main>
-      <section className='embedded-section'>
-        <h1>{content[language].EmbeddedSystems.title}</h1>
-        <div className='content-table-container'>
-          <h2>
-            {content[language].EmbeddedSystems.subtitle}
-            <span> {content[language].EmbeddedSystems.support} </span>
-            {content[language].EmbeddedSystems.subtitle2}
-          </h2>
-          <table className='embedded-table1'>
-            <tbody>
-              <tr>
-                <td className="container-check">
-                  <Check width={screenWidth < screenSizes.tablet ? 32 : 44} height={screenWidth < screenSizes.tablet ? 32 : 44} value={"check-1"}/>
-                </td>
-                <td>{content[language].EmbeddedSystems.table1.application}</td>
-              </tr>
-              <tr>
-                <td className="container-check">
-                  <Check width={screenWidth < screenSizes.tablet ? 32 : 44} height={screenWidth < screenSizes.tablet ? 32 : 44} value={"check-2"}/>
-                </td>
-                <td>{content[language].EmbeddedSystems.table1.industry}</td>
-              </tr>
-              <tr>
-                <td className="container-check">
-                  <Check width={screenWidth < screenSizes.tablet ? 32 : 44} height={screenWidth < screenSizes.tablet ? 32 : 44} value={"check-3"}/>
-                </td>
-                <td>{content[language].EmbeddedSystems.table1.standards}</td>
-              </tr>
-              <tr>
-                <td className="container-check">
-                  <Check width={screenWidth < screenSizes.tablet ? 32 : 44} height={screenWidth < screenSizes.tablet ? 32 : 44} value={"check-4"}/>
-                </td>
-                <td>{content[language].EmbeddedSystems.table1.dimensions}</td>
-              </tr>
-              <tr>
-                <td className="container-check">
-                  <Check width={screenWidth < screenSizes.tablet ? 32 : 44} height={screenWidth < screenSizes.tablet ? 32 : 44} value={"check-5"}/>
-                </td>
-                <td>{content[language].EmbeddedSystems.table1.weight}</td>
-              </tr>
-              <tr>
-                <td className="container-check">
-                  <Check width={screenWidth < screenSizes.tablet ? 32 : 44} height={screenWidth < screenSizes.tablet ? 32 : 44} value={"check-6"}/>
-                </td>
-                <td>{content[language].EmbeddedSystems.table1.cost}</td>
-              </tr>
-              <tr>
-                <td className="container-check">
-                  <Check width={screenWidth < screenSizes.tablet ? 32 : 44} height={screenWidth < screenSizes.tablet ? 32 : 44} value={"check-7"}/>
-                </td>
-                <td>{content[language].EmbeddedSystems.table1.lifetime}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+{/*       <nav className='nav-embedded'>
+        <ul>
+          <li>CONCEPT & SPECIFICATION</li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav> */}
+      <FirstSection/>
+      <section className='embedded-secondSection'>
+        <h2>Taking <span>all variables</span> into account from day one</h2>
       </section>
-{/*         <div className='container-img'>
-          <Image src={embedded1} alt='PCB' fill={true}/>
-        </div> */}
       {
-        screenWidth >= screenSizes.laptop &&
+        screenWidth >= screenSizes.tablet &&
           <div className='scroll'>
             <p>{content[language].EmbeddedSystems.scroll}</p>
             <span></span>
