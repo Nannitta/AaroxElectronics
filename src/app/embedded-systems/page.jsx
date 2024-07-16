130aee854a18e928fb8284dd7fdc42c88a8fd397
Nanna <nanaa.gonzalez@gmail.com>
2024-07-16 15:30:18 +0200

    Add secondSection to embedded page

--- src/app/embedded-systems/page.jsx
+++ src/app/embedded-systems/page.jsx
@@ -1,15 +1,13 @@
 'use client';
 
-import { useLanguageStore } from '../stores/languageStore';
-import content from '../content.json';
 import './embedded.css';
-import CheckWindowWidth from '../hooks/useWindowWidth';
-import { screenSizes } from '../lib/screenSizes';
 import FirstSection from './firstSection/FirstSection';
+import SecondSection from './secondSection/SecondSection';
+import { useRef } from 'react';
+import ScrollButton from './scroll/ScrollButton';
 
 export default function EmbeddedSystems() {
-  const language = useLanguageStore((state) => state.language);
-  const { screenWidth } = CheckWindowWidth();
+  const secondSectionRef = useRef(null);
 
   return(
     <main>
@@ -25,16 +23,8 @@ export default function EmbeddedSystems() {
         </ul>
       </nav> */}
       <FirstSection/>
-      <section className='embedded-secondSection'>
-        <h2>Taking <span>all variables</span> into account from day one</h2>
-      </section>
-      {
-        screenWidth >= screenSizes.tablet &&
-          <div className='scroll'>
-            <p>{content[language].EmbeddedSystems.scroll}</p>
-            <span></span>
-          </div>
-      }
+      <SecondSection refSection={secondSectionRef}/>
+      <ScrollButton refSection={secondSectionRef}/>
     </main>
   )
 }
