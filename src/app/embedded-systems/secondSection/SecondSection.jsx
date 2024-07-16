8f0c0853c16ab304fee5f1cbca6319e0128db7c7
Nanna <nanaa.gonzalez@gmail.com>
2024-07-16 20:44:26 +0200

    WIP scroll transitions

--- src/app/embedded-systems/secondSection/SecondSection.jsx
+++ src/app/embedded-systems/secondSection/SecondSection.jsx
@@ -1,26 +1,70 @@
 import { useLanguageStore } from '../../stores/languageStore';
 import content from '../../content.json';
-import Image from 'next/image';
-import pyramid1 from '../../assets/images/embedded/pyramid/pyramid1.webp';
-import pyramid2 from '../../assets/images/embedded/pyramid/pyramid2.webp';
-import pyramid3 from '../../assets/images/embedded/pyramid/pyramid3.webp';
-import pyramid4 from '../../assets/images/embedded/pyramid/pyramid4.webp';
-import pyramid5 from '../../assets/images/embedded/pyramid/pyramid5.webp';
 import './secondSection.css';
+import { useEffect, useState } from 'react';
 
 export default function SecondSection({ refSection }) {
   const language = useLanguageStore((state) => state.language);
+  const [embeddedSection, setEmbeddedSection] = useState(null);
+  const [containerPyramid, setContainerPyramid] = useState(null);
+  const totalFrames = 5;
 
-  return(
-    <section className='embedded-secondSection scroll-section' ref={refSection}>
-      <h2>{content[language].EmbeddedSystems.secondSection.text1} <span>{content[language].EmbeddedSystems.secondSection.texthighlighted}</span> {content[language].EmbeddedSystems.secondSection.text2}</h2>
-      <div className='container-pyramid'>
-        <Image src={pyramid5} alt='Cost' fill={true}/>
-        <Image src={pyramid4} alt='Manufacturing' fill={true}/>
-        <Image src={pyramid3} alt='Simplicity' fill={true}/>
-        <Image src={pyramid2} alt='EMC Emission' fill={true}/>
-        <Image src={pyramid1} alt='Performance' fill={true}/>
+  useEffect(() => {
+    const start = () => {
+      const embeddedSectionSelected = document.querySelector(".embedded-secondSection");
+      const containerPyramidSelected = document.querySelector(".container-pyramid");
+      setEmbeddedSection(embeddedSectionSelected);
+      setContainerPyramid(containerPyramidSelected);
+
+      if (!embeddedSection || !containerPyramid) return;
+
+      function preloadImages() {
+        for (let i = 1; i <= totalFrames; i++) {
+          const img = new Image();
+          img.src = currentFrame(i);
+        }
+      }
+
+      preloadImages();
+      window.addEventListener("scroll", getFrameRates);
+    };
+
+    start();
+
+    return () => {
+      window.removeEventListener("scroll", getFrameRates);
+    };
+  });
+
+  function currentFrame(frame) {
+    return `./embedded/pyramid/pyramid${frame}.webp`;
+  }
+
+  function getFrameRates() {
+    const rect = embeddedSection.getBoundingClientRect();
+    const positiveTop = rect.top <= 0 ? Math.abs(rect.top) : 0;
+    const finalPercentage = Math.floor((totalFrames * positiveTop) / 100);
+    let frames = finalPercentage <= totalFrames ? finalPercentage : totalFrames;
+
+    if (!frames) frames = 1;
+
+    const img = new Image();
+    img.src = currentFrame(frames);
+    img.onload = () => {
+      containerPyramid.style.backgroundImage = `url(${img.src})`;
+    };
+  }
+
+  return (
+    <section className="embedded-secondSection" ref={refSection}>
+      <h2>
+        {content[language].EmbeddedSystems.secondSection.text1}{' '}
+        <span>{content[language].EmbeddedSystems.secondSection.texthighlighted}</span>{' '}
+        {content[language].EmbeddedSystems.secondSection.text2}
+      </h2>
+      <div className="container-pyramid">
+        <canvas id="canvas"></canvas>
       </div>
     </section>
-  )
+  );
 }
