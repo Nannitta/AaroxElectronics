3fd5ee63eaa849fcf243070465e12d227512c6c7
Nanna <nanaa.gonzalez@gmail.com>
2024-07-18 12:03:08 +0200

    Fix some styles from embedded page

--- src/app/embedded-systems/thirdSection/ThirdSection.jsx
+++ src/app/embedded-systems/thirdSection/ThirdSection.jsx
@@ -1,57 +1,18 @@
-import { useEffect } from 'react';
-import content from '../../content.json';
+import TechCarousel from './TechCarousel';
 import { useLanguageStore } from '../../stores/languageStore';
-import pcbBG from '../../assets/images/embedded/pcbBG.webp';
-import Image from 'next/image';
-import { screenSizes } from '../../lib/screenSizes';
-import CheckWindowWidth from '../../hooks/useWindowWidth';
+import content from '../../content.json';
 import './thirdSection.css';
 
 export default function ThirdSection() {
   const language = useLanguageStore((state) => state.language);
-  const { screenWidth } = CheckWindowWidth();
 
-  useEffect(() => {
-    const handleScroll = () => {
-      const section = document.querySelector('.embedded-thirdSection');
-      const textElements = section.querySelectorAll('h2, h3');
-      const imageContainer = section.querySelector('.pcb-bg-container');
-      
-      const rect = section.getBoundingClientRect();
-      const sectionTop = rect.top;
-      const windowHeight = window.innerHeight;
-
-      if (sectionTop <= 0 && sectionTop >= -windowHeight) {
-        const opacity = 1 + ((sectionTop / windowHeight) * 5);
-        textElements.forEach(el => {
-          el.style.opacity = opacity;
-        });
-        if(screenWidth < screenSizes.laptop) {
-          imageContainer.style.transform = `translateY(${(sectionTop / 4) / 2}px)`;
-        } else {
-          imageContainer.style.transform = `translateY(${sectionTop / 2.5}px)`;
-        }
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [screenWidth]);
-
-  return (
+  return(
     <section className='embedded-thirdSection'>
-      <h2>{content[language].EmbeddedSystems.thirdSection.title}</h2>
+      <h2>{content[language].EmbeddedSystems.thirdSection.title} <span>{content[language].EmbeddedSystems.thirdSection.design}</span></h2>
       <h3>
-        {content[language].EmbeddedSystems.thirdSection.text1}
-        <span> {content[language].EmbeddedSystems.thirdSection.tested} </span>
-        <p>{content[language].EmbeddedSystems.thirdSection.and}</p>
-        <span> {content[language].EmbeddedSystems.thirdSection.proven} </span>
+        {content[language].EmbeddedSystems.thirdSection.subtitle}
       </h3>
-      <div className='pcb-bg-container'>
-        <Image src={pcbBG} alt="Black and white pcb" fill={true}/>
-      </div>
+      <TechCarousel/>
     </section>
-  );
+  )
 }
