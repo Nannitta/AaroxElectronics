c00625014ad1ab1e988ea178e1a1a35c8dce4634
Nanna <nanaa.gonzalez@gmail.com>
2024-07-30 11:26:44 +0200

    Add transition to firstSection on power electronics page

---
+++ src/app/lib/scrollPowerElectronicsFirstSection.js
@@ -0,0 +1,70 @@
+export default function handleColorOpacity() {
+  const scrollPosition = window.scrollY;
+  const power = document.querySelector('#power');
+  const battery = document.querySelector('#battery');
+  const ev = document.querySelector('#ev');
+  const wireless = document.querySelector('#wireless');
+  const bms = document.querySelector('#bms');
+  const inverters = document.querySelector('#inverters');
+  const controls = document.querySelector('#controls');
+  const lighting = document.querySelector('#lighting');
+
+  const containerDefs = document.querySelector('.container-defs');
+  const containerSmallDashboard = document.querySelector('.container-smallDashboard');
+
+  if (scrollPosition >= 1500) {
+    containerDefs.style.transform = 'translateX(200%)';
+    containerSmallDashboard.style.transform = 'translateX(70%)';
+  } else {
+    containerDefs.style.transform = 'translateX(0)';
+    containerSmallDashboard.style.transform = 'translateX(0)';
+  }
+
+  if(scrollPosition >= 0 && scrollPosition <= 188) {
+    power.classList.add('span-highlighted')
+  } else {
+    power.classList.remove('span-highlighted')
+  }
+
+  if(scrollPosition >= 188 && scrollPosition <= 376) {
+    battery.classList.add('span-highlighted')
+  } else {
+    battery.classList.remove('span-highlighted')
+  }
+
+  if(scrollPosition >= 376 && scrollPosition <= 564) {
+    ev.classList.add('span-highlighted')
+  } else {
+    ev.classList.remove('span-highlighted')
+  }
+  
+  if(scrollPosition >= 564 && scrollPosition <= 752) {
+    wireless.classList.add('span-highlighted')
+  } else {
+    wireless.classList.remove('span-highlighted')
+  }
+  
+  if(scrollPosition >= 752 && scrollPosition <= 940) {
+    bms.classList.add('span-highlighted')
+  } else {
+    bms.classList.remove('span-highlighted')
+  }
+  
+  if(scrollPosition >= 940 && scrollPosition <= 1128) {
+    inverters.classList.add('span-highlighted')
+  } else {
+    inverters.classList.remove('span-highlighted')
+  }
+
+  if(scrollPosition >= 1128 && scrollPosition <= 1316) {
+    controls.classList.add('span-highlighted')
+  } else {
+    controls.classList.remove('span-highlighted')
+  }
+
+  if(scrollPosition >= 1316) {
+    lighting.classList.add('span-highlighted')
+  } else {
+    lighting.classList.remove('span-highlighted')
+  }
+}
--- src/app/power-electronics/firstSection/FirstSection.jsx
+++ src/app/power-electronics/firstSection/FirstSection.jsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import smallDashboard from '../../assets/images/power/smallDashboard.webp';
 import './firstSection.css';
 import { useEffect } from 'react';
+import handleColorOpacity from '../../lib/scrollPowerElectronicsFirstSection';
 
 const passion = Passion_One({
   weight: '400',
@@ -15,69 +16,10 @@ export default function FirstSection() {
   const language = useLanguageStore((state) => state.language);
 
   useEffect(() => {
-    const power = document.querySelector('#power');
-    const battery = document.querySelector('#battery');
-    const ev = document.querySelector('#ev');
-    const wireless = document.querySelector('#wireless');
-    const bms = document.querySelector('#bms');
-    const inverters = document.querySelector('#inverters');
-    const controls = document.querySelector('#controls');
-    const lighting = document.querySelector('#lighting');
-    
-    const handleScrollPosition = () => {
-      const scrollPosition = window.scrollY;
-
-      if(scrollPosition >= 0 && scrollPosition <= 188) {
-        power.classList.add('span-highlighted')
-      } else {
-        power.classList.remove('span-highlighted')
-      }
-
-      if(scrollPosition >= 188 && scrollPosition <= 376) {
-        battery.classList.add('span-highlighted')
-      } else {
-        battery.classList.remove('span-highlighted')
-      }
-
-      if(scrollPosition >= 376 && scrollPosition <= 564) {
-        ev.classList.add('span-highlighted')
-      } else {
-        ev.classList.remove('span-highlighted')
-      }
-      
-      if(scrollPosition >= 564 && scrollPosition <= 752) {
-        wireless.classList.add('span-highlighted')
-      } else {
-        wireless.classList.remove('span-highlighted')
-      }
-      
-      if(scrollPosition >= 752 && scrollPosition <= 940) {
-        bms.classList.add('span-highlighted')
-      } else {
-        bms.classList.remove('span-highlighted')
-      }
-      
-      if(scrollPosition >= 940 && scrollPosition <= 1128) {
-        inverters.classList.add('span-highlighted')
-      } else {
-        inverters.classList.remove('span-highlighted')
-      }
-
-      if(scrollPosition >= 1128 && scrollPosition <= 1316) {
-        controls.classList.add('span-highlighted')
-      } else {
-        controls.classList.remove('span-highlighted')
-      }
-
-      if(scrollPosition >= 1316) {
-        lighting.classList.add('span-highlighted')
-      } else {
-        lighting.classList.remove('span-highlighted')
-      }
-    }
-
-    window.addEventListener('scroll', handleScrollPosition);
-    return () => window.removeEventListener('scroll', handleScrollPosition);
+    handleColorOpacity();
+ 
+    window.addEventListener('scroll', handleColorOpacity);
+    return () => window.removeEventListener('scroll', handleColorOpacity);
   }, []);
 
   return (
