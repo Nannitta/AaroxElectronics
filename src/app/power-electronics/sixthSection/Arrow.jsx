0928b37b6d2b115c9b36c8fedba51e4b1f30461a
Nanna <nanaa.gonzalez@gmail.com>
2024-08-06 14:36:42 +0200

    Add arrow animation on scroll

--- src/app/power-electronics/sixthSection/Arrow.jsx
+++ src/app/power-electronics/sixthSection/Arrow.jsx
@@ -1,11 +1,53 @@
-export default function Arrow({ width, height }) {
-  return(
-    <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 740 2287" width={width} height={height}>
-      <path d="M361,2287l-11-18h22L361,2287z" style={{opacity: "1"}} fill='#2bac44'></path>
-      <path d="M360.9,2269c0,0-8.6-87.8,57.4-145.5s314.4-188.1,314.4-421.5S676,1344.9,372,1234.9S0,1000,0,846.2
-        S86.5,564,315.7,486.7S786.5,333.9,722,1" fill='none' stroke='#edfff5' strokeMiterlimit={10} strokeWidth={5}></path>
-      <path d="M360.9,2269c0,0-8.6-87.8,57.4-145.5s314.4-188.1,314.4-421.5S676,1344.9,372,1234.9S0,1000,0,846.2
-        S86.5,564,315.7,486.7S786.5,333.9,722,1" fill='none' stroke='#2bac44' strokeWidth={2} strokeLinecap='round' strokeLinejoin='round' strokeMiterlimit={10} strokeDasharray={[1.999, 5.997]}></path>
-  </svg>
-  )
+export default function Arrow({ className, strokeDashoffset }) {
+  return (
+    <svg
+      version="1.1"
+      width="740"
+      height="2287"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      xmlSpace="preserve"
+      viewBox="0 0 740 2287"
+      className={className}
+    >
+      <style type="text/css">
+        {`
+          .st01 {
+            fill: #2BB065;
+          }
+          .st11 {
+            fill: none;
+            stroke: #EDFFF5;
+            stroke-miterlimit: 10;
+            stroke-width: 5;
+          }
+          .st21 {
+            fill: none;
+            stroke: #2BB065;
+            stroke-width: 2;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+            stroke-miterlimit: 10;
+            stroke-dasharray: 1.999, 5.997;
+          }
+        `}
+      </style>
+      <path
+        className="st01"
+        d="M361,2287l-11-18h22L361,2287z"
+        style={{ opacity: 1 }}
+      />
+      <path
+        className="st21"
+        d="M360.9,2269c0,0-8.6-87.8,57.4-145.5s314.4-188.1,314.4-421.5S676,1344.9,372,1234.9S0,1000,0,846.2
+        S86.5,564,315.7,486.7S786.5,333.9,722,1"
+      />
+      <path
+        className="st11"
+        d="M360.9,2269c0,0-8.6-87.8,57.4-145.5s314.4-188.1,314.4-421.5S676,1344.9,372,1234.9S0,1000,0,846.2
+        S86.5,564,315.7,486.7S786.5,333.9,722,1"
+        style={{ strokeDashoffset }}
+      />
+    </svg>
+  );
 }
